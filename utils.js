f00d4174623ce8edca58b1816d01ea3e43507543
emmadenney <emma_denney@hotmail.com>
2023-05-15 10:41:52 +0100

    sort-method

--- utils.js
+++ utils.js
@@ -23,111 +23,46 @@ function mergeProducts(products) {
   });
 }
 
-function sortProducts(unsortedProducts) {
-  // calc mid index of array
-  const mid =
-    unsortedProducts.length % 2 === 0
-      ? unsortedProducts.length / 2
-      : (unsortedProducts.length - 1) / 2;
+function sortProducts(products) {
+  products.sort((a, b) => {
+    const bayA = a.pick_location[0];
+    const bayB = b.pick_location[0];
 
-  const sortedProducts = [unsortedProducts[mid]];
-  let newMid = 0;
-
-  // sort left half
-  for (let i = 0; i < mid; i++) {
-    const bay = unsortedProducts[i].pick_location[0];
-    if (bay.length === 1) {
-      if (bay[0] > unsortedProducts[mid].pick_location[0]) {
-        sortedProducts.push(unsortedProducts[i]);
-      } else {
-        sortedProducts.unshift(unsortedProducts[i]);
-        newMid++;
-      }
-    } else {
-      if (bay[1] > unsortedProducts[mid].pick_location[0][1]) {
-        sortedProducts.push(unsortedProducts[i]);
-      } else {
-        sortedProducts.unshift(unsortedProducts[i]);
-        newMid++;
-      }
+    // if both bays are not the same length, sort by length
+    if (bayA.length !== bayB.length) {
+      return bayA.length - bayB.length;
     }
-  }
 
-  // sort right half
-  for (let i = mid + 1; i < unsortedProducts.length; i++) {
-    const bay = unsortedProducts[i].pick_location[0];
-    if (bay.length > 1) {
-      if (bay[0] > unsortedProducts[mid].pick_location[0]) {
-        sortedProducts.push(unsortedProducts[i]);
-      } else {
-        sortedProducts.unshift(unsortedProducts[i]);
-        newMid++;
-      }
+    // else sort by first letter if they are not the same letter
+    // if both bays are single char, or if both are double but the first char is not the same, sort by first char
+    // localeCompare
+    if (bayA.length === 1 || bayA[0] !== bayB[0]) {
+      return bayA[0].localeCompare(bayB[0]);
+      // else sort by second letter
     } else {
-      if (bay[1] > unsortedProducts[mid].pick_location[0][1]) {
-        sortedProducts.push(unsortedProducts[i]);
-      } else {
-        sortedProducts.unshift(unsortedProducts[i]);
-        newMid++;
-      }
+      return bayA[1].localeCompare(bayB[1]);
     }
-  }
+  });
 
-  if (unsortedProducts.length > 2) {
-    return [
-      ...sortProducts(sortedProducts.slice(0, newMid)),
-      ...sortProducts(sortedProducts.slice(newMid)),
-    ];
-  }
-
-  return sortedProducts;
+  return products;
 }
 
 function updateProducts(products) {
   const mergedProducts = mergeProducts(products);
 
-  // split pick locations into bay and shelves and convert bay letters to ASCII
+  // split pick location string into bay and shelf
   const convertedLocations = mergedProducts.map((product) => {
     product.pick_location = product.pick_location.split(" ");
-    product.pick_location[0] = product.pick_location[0]
-      .split("")
-      .map((letter) => {
-        return letter.charCodeAt();
-      });
     return product;
   });
 
-  // sort into bays A - Z and bays AA = AZ
-  const singleLetterBays = [];
-  const doubleLetterBays = [];
-
-  for (let i = 0; i < convertedLocations.length; i++) {
-    if (convertedLocations[i].pick_location[0].length === 1) {
-      singleLetterBays.push(convertedLocations[i]);
-    } else {
-      doubleLetterBays.push(convertedLocations[i]);
-    }
-  }
-
-  const sortedByBay = [];
-
-  if (singleLetterBays.length > 0) {
-    sortedByBay.push(...sortProducts(singleLetterBays));
-  }
-
-  if (doubleLetterBays.length > 0) {
-    sortedByBay.push(...sortProducts(doubleLetterBays));
-  }
+  const sortedByBay = sortProducts(convertedLocations);
 
   // sort shelf numbers here
   // const sortedByShelf = sortProducts(sortedByBay);
 
-  // convert ASCII codes back to letters
+  // join bay and shelf elements back into a string
   const updatedProducts = sortedByBay.map((product) => {
-    product.pick_location[0] = product.pick_location[0].map((letter) => {
-      return String.fromCharCode(letter);
-    });
-    product.pick_location[0] = product.pick_location[0].join("");
     product.pick_location = product.pick_location.join(" ");
     return product;
   });
